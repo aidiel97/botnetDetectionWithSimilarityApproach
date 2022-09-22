1f17ba983a179f398275656dff09c85eb8fcbf18
aidiel <aidielrachman@gmail.com>
2022-09-23 04:25:18 +0700

    print loading

--- src/detector.py
+++ src/detector.py
@@ -192,8 +192,8 @@ def reportDocumentation(tpfp='tp', dataset='ctu', detail=7, threshold=0.5):
 
   recoredReport = []
   for datasetDetail in range(1, len(listSubDataset)+1):
+    print('\t..Getting Data from '+stringDatasetName+datasetDetail)
     for threshold in range(0,10):
-      print('\t..Getting Data from '+stringDatasetName+datasetDetail+'with threshold = '+str(threshold/10))
       recoredReport.append(getReportData('tp', stringDatasetName, datasetDetail, threshold/10))
       recoredReport.append(getReportData('fp', stringDatasetName, datasetDetail, threshold/10))
 
